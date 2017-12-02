d05a170c8a41bbe32325b92ce9ea4a25c2e1a46a
mixyznik <mixy80@gmail.com>
2017-12-02 13:46:20 +0100

    new final pomodorist

---
+++ js/pomodorist.js
@@ -0,0 +1,33 @@
+var y;
+
+function stop() {
+    clearInterval(y);
+}
+
+var sekund = 60;
+var minut = 24;
+
+function count() {
+
+    document.getElementById("zemo").innerHTML = --sekund;
+    document.getElementById("stojko").innerHTML = minut;
+    if (sekund == 1) {
+        sekund = 60;
+        minut = minut - 1;
+    }
+    if (minut < 0) {
+        clearInterval(y);
+        document.getElementById("zemo").innerHTML = "Kraj";
+        document.getElementById("stojko").innerHTML = "Kraj";
+    }
+
+}
+
+function start() {
+
+    y = setInterval(count, 1000);
+}
+
+function reset() {
+    location.reload(true);
+}
